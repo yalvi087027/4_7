e10999298835f4c3a66778412bcfb0a9935b3a47
yalvi087027 <75536343+yalvi087027@users.noreply.github.com>
2020-12-28 08:54:43 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+const int N = 5;
+
+int func(int x, int y)
+{
+    int a = 1;
+    while(pow(a, y) - x < 0)
+    {
+        if(pow(a,y)==x)
+        {
+            return a;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n,m;
+    scanf("%d %d",&n,&m);
+    int k = func(n,m);
+    if(k==-1)
+    {
+        printf("net");
+    }
+    else{
+        printf("da, %d v dyepeni %d ravno %d",k,m,n);
+    }
+}
